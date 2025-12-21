541363437dcb32766e0abda73f695635db89b33f
twocash <jimcalhoun@gmail.com>
2025-12-21 10:11:57 -0800

    chore: final cleanup

--- cleanup2.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d C:\GitHub\the-grove-foundation
-del commit-skill.bat
-git add -A
-git commit -m "chore: cleanup temp files"
-git push
-del cleanup2.bat
---
+++ final.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d C:\GitHub\the-grove-foundation
+git add -A
+git commit -m "chore: final cleanup"
+git push
